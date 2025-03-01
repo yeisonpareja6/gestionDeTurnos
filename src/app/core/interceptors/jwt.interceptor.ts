41f3369f7e00b82afefd4954e60a0eed418a6094
yeison6 <yeison@innclod.com>
2025-02-28 20:28:47 -0500

    Crear interceptor

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -1,5 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  // const messageService = inject(MessageService);
+  const token = JSON.parse(localStorage.getItem('token')!);
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+  return next(req).pipe(
+    catchError((error) => {
+      return throwError(() => { console.log('Aca va el servicio para mostrar el error');
+       });
+    })
+  )
 };
