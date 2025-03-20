b8eb04186ee308e18f4b7e88ae13860f39ae9c1b
yeison6 <yeison@innclod.com>
2025-03-20 07:02:40 -0500

    Agregar rutas principales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,28 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    },
+    {
         path: 'auth',
         loadChildren: () => import('./components/auth/auth.routes').then(a => a.authRoutes)
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => import('./components/dashboard/dashboard.routes').then(d => d.dashboardRoute)
+    },
+    {
+        path: 'workManagement',
+        loadChildren: () => import('./components/work-management/workManagement.routes').then(w => w.workManagementRoutes)
+    },
+    {
+        path: 'workArea',
+        loadChildren: () => import('./components/work-area/work-area.routes').then(w => w.routesWorkAreas)
     },
     {
         path: '**',
         redirectTo: 'auth'
     }
-    // {
-    // path: '',
-    // loadComponent: () => import('./app.component')
-    // children: [
-    // {
-    // path: '**',
-    // redirectTo: 'dashboard'
-    // }
-    // ]
-    // }
 ];
