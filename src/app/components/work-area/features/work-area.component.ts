38b02cad7ff018ab4b38cf8041d14bc6e6441cc4
yeison6 <yeison@innclod.com>
2025-03-20 18:16:46 -0500

    Crear componente workArea

--- src/app/components/work-area/features/work-area.component.ts
+++ src/app/components/work-area/features/work-area.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { WorkAreaService } from '../services/work-area.service';
 
 @Component({
   selector: 'app-work-area',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './work-area.component.html',
   styleUrl: './work-area.component.scss'
 })
-export class WorkAreaComponent {
+export class WorkAreaComponent implements OnInit {
+
+  WorkAreaService = inject(WorkAreaService);
+
+  ngOnInit(): void {
+    // debugger
+    this.WorkAreaService.getWorkArea().subscribe({ next: (res) => res});
+  }
 
 }
