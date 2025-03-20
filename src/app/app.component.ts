ad1d2fc5082b2371a24d4f99c103ef59c532aa5a
yeison6 <yeison@innclod.com>
2025-03-20 07:01:41 -0500

    Configuracion para mostrar o no el header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gestionTurnos';
+
+  isLogin = signal(false);
+  router = inject(Router);
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      if (this.router.url === '/auth') {
+        this.isLogin.set(false);
+      } else {
+        this.isLogin.set(true);
+      }
+    });
+  }
+
 }
