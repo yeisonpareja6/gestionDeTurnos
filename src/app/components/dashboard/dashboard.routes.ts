9f4f54fb8483de52811c1a90b968860c50ad36f2
yeison6 <yeison@innclod.com>
2025-03-20 07:05:32 -0500

    Agregar ruta de pagina principal

---
+++ src/app/components/dashboard/dashboard.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard.component";
+
+export const dashboardRoute: Routes = [
+    {
+        path: '',
+        component: DashboardComponent
+    }
+]
