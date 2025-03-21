920bdc7dc87dfb69eea021e8e53f286af25f127a
yeison6 <yeison@innclod.com>
2025-03-21 08:03:22 -0500

    Eliminar componente dashboard

--- src/app/components/dashboard/dashboard.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from "@angular/router";
-import { DashboardComponent } from "./dashboard.component";
-
-export const dashboardRoute: Routes = [
-    {
-        path: '',
-        component: DashboardComponent
-    }
-]
